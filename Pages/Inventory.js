a39514f23c0a47fb8f966e6e89de91fd6fa55dd5
Tamera Middlebrooks <105788615+tameramiddlebrooks@users.noreply.github.com>
2023-01-26 04:13:50 -0500

    fixed Inventory page

--- Pages/Inventory.js
+++ Pages/Inventory.js
@@ -1,52 +1,53 @@
-function Inventory () {
-
 import React, { useState } from 'react'
-
+    
 // import ReactDOM from 'react-dom'
+    
+    function Inventory () {
+    
+        const [Input, updateForm] = React.useState(initialData)
+    
+        const handleChange = (e) => {
+            updateForm({
+                ...Input,
+                [e.target.name]: e.target.value.trim()
+            })
+        };
+    
+        const handleSubmit = (e) => {
+            e.preventDefault()
+            console.log(Input);
+        };
+    
+        return (
+            <>
+            <p className="directions">
+            Use the form below to update your inventory. Update 1 item at a time and check the console to view your updates.
+                </p>
+    
+            {/* item 1 */}
+            <label className="label1">
+                Item
+                <input name="Item" className="the-item" onChange={handleChange} />
+                </label>
+                <br />
+    
+                <label className="label2">
+                    Quantity
+                    <input name="Quantity" className="the-quantity" onChange={handleChange} />
+                </label>
+                <br />
+                <button className="update-button" onClick={handleSubmit}>Submit Update</button>
+    
+            </>
+    
+        )
+    }
 
-function Inventory () {
+    
+    const initialData = Object.freeze({
+        Item: "",
+        Quantity: ""
+    })
 
-    const [Input, updateForm] = React.useState(initialData)
-
-    const handleChange = (e) => {
-        updateForm({
-            ...Input,
-            [e.target.name]: e.target.value.trim()
-        })
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        console.log(Input);
-    };
-
-    return (
-        <>
-        <p className="directions">
-        Use the form below to update your inventory. Update 1 item at a time and check the console to view your updates.
-            </p>
-
-        {/* item 1 */}
-        <label className="label1">
-            Item
-            <input name="Item" className="the-item" onChange={handleChange} />
-            </label>
-            <br />
-
-            <label className="label2">
-                Quantity
-                <input name="Quantity" className="the-quantity" onChange={handleChange} />
-            </label>
-            <br />
-
-        </>
-
-    )
-}
-
-const initialData = Object.freeze({
-    Item: "",
-    Quantity: ""
-})
-
-export default Inventory;
+    
+    export default Inventory;
